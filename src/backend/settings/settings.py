b7257d4038b06834de7497a6dbc8da75f1c52298
SofiaKSBarros <kedu.barros@gmail.com>
2026-02-20 01:33:23 -0300

    🐛 fix: using secret from .env since it was generating randomly and not the one set

--- src/backend/settings/settings.py
+++ src/backend/settings/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-d!l56$i&6#do2u6ozj02j=ecf5ot7x#fk*dbt-y)n^#d$2$*gw'
+SECRET_KEY = str(os.getenv("SECRET_KEY"))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
